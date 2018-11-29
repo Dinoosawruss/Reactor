8176590b7f116a501b1c06f0106b6287e6e3b38e
Dinoosawruss <m.dormand@sky.com>
2018-11-29 20:42:08 +0000

    :white_check_mark: Added main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+#Discord Imports
+import discord
+from discord.ext import commands
+
+#Other Imports
+import json
+
+#Requirements
+TOKEN = 'X'
+prefix = "-"
+bot = commands.Bot(command_prefix=prefix)
+bot.remove_command("help")
+
+#Other main vars
+#serversCount = len(bot.guild) + 1
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    #await bot.change_presence(game=discord.Game(name="Used by " + str(serversCount) + " servers!"))
+
+#General Bot
+@bot.command(pass_context=True)
+async def help(ctx):
+    embedC = 0xff0000
+    embed = discord.Embed(title="**- Commands -**", description="", color=embedC)
+    embed.add_field(name="{}help".format(prefix), value="Shows this message", inline=False)
+    await ctx.send(embed=embed)
+
+@bot.event
+async def on_message(message):
+    if not message.author.bot:
+        with open('reactions.json') as u:
+            data = json.load(u)
+
+        for user in data['users']:
+            name = user['name']
+            toAdd = user['react']
+            if str(name) == str(message.author):
+                await message.add_reaction(toAdd)
+
+    await bot.process_commands(message)
+
+bot.run(TOKEN)
